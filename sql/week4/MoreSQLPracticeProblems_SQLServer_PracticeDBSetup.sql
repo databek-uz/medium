babd07f6b7b576923e54ac43320e7436cfd0ba90
Utkir Khujaev <balkibumen@gmail.com>
2026-03-23 15:56:08 +0500

    week 4

--- sql/week4/MoreSQLPracticeProblems_SQLServer_PracticeDBSetup.sql
+++ sql/week4/MoreSQLPracticeProblems_SQLServer_PracticeDBSetup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfabab5721228ab02bf22a3c494fa4659ca116f33a2337c06b0312d1f381e1d9
-size 11252292
+oid sha256:c619906a57de4e76b0182044dd4bbea05db5419472919dbd0cbb893ebc380d0d
+size 11252396
--- sql/week4/Part2_SQLPracticeProblems_SQLServer_PracticeDBSetup.sql
+++ sql/week4/Part2_SQLPracticeProblems_SQLServer_PracticeDBSetup.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c961ec4eeca226b16a9bd26f3324bcbc6971f7a47952f50738c56c20f4da747b
-size 1503478
+oid sha256:3a638890d23a338c760fbdc01502756486fe88fdb732886258aec9c815cc48d1
+size 1503524
